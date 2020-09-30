66c263f24e1df5c3bddbac7f9ee870db27e64985
gokulstevee <gokulstevee@gmail.com>
2020-09-30 14:25:30 +0530

    brain_of_the_app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import { toast, ToastContainer } from "react-toastify";
+import { Container, Row, Col } from "reactstrap";
+
+import BuyPage from "./Components/BuyPage";
+import Cart from "./Components/Cart";
+
+const App = () => {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addInCart = (item) => {
+    const isAlreadyAdded = cartItem.findIndex(function (array) {
+      return array.id === item.id;
+    });
+
+    if (isAlreadyAdded !== -1) {
+      toast("Already added to cart", {
+        type: "warning",
+      });
+      return;
+    }
+
+    setCartItem([...cartItem, item]);
+  };
+
+  const buyNow = () => {
+    setCartItem([]);
+
+    toast("Purchase Complete", {
+      type: "success",
+    });
+  };
+
+  const removeItem = (item) => {
+    setCartItem(cartItem.filter((singleItem) => singleItem.id !== item.id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <ToastContainer />
+      <Row>
+        <Col md="8">
+          <BuyPage addInCart={addInCart} />
+        </Col>
+        <Col className="mt-2 mb-2" md="4">
+          <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
